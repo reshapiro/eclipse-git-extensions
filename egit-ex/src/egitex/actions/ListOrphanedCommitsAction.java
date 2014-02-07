878ac742afd25a7b5c3cb75b51993ab958efdbbb
R Shapiro <rshapiro@bbn.com>
2014-02-07 12:41:53 -0500

    Warn about potential slowness of finding orphan commits

--- egit-ex/src/egitex/actions/ListOrphanedCommitsAction.java
+++ egit-ex/src/egitex/actions/ListOrphanedCommitsAction.java
@@ -2,6 +2,10 @@ package egitex.actions;
 
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
+import egit_ex.util.MissingRequiredParameterException;
+import egit_ex.util.ParameterSet;
+import egit_ex.util.PromptCancelledException;
+
 /**
  * Execute the Git operation that will list orphaned commits
  * 
@@ -12,12 +16,17 @@ public class ListOrphanedCommitsAction
       extends GitAction
 
       implements IWorkbenchWindowActionDelegate {
+   
    private static final String[] ARGS = new String[] {
       "fsck", "--lost-found"
    };
+   
+   private static final ParameterSet PARAMETERS = new ParameterSet("Finding orphan commits is potentially slow. Proceed?");
 
    @Override
-   String[] getArgs() {
+   String[] getArgs() throws
+         PromptCancelledException, MissingRequiredParameterException {
+      promptForParameters(PARAMETERS, ARGS);
       return ARGS;
    }
 
