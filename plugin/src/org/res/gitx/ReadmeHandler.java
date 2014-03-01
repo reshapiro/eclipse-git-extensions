cf524029a7fe984ece303d4a8c7bed3fa8130fc3
R Shapiro <rshapiro@bbn.com>
2014-03-01 11:12:49 -0500

    Add a readme menu item

--- plugin/src/org/res/gitx/ReadmeHandler.java
+++ plugin/src/org/res/gitx/ReadmeHandler.java
@@ -3,27 +3,26 @@ package org.res.gitx;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-class ReadmeHandler
+public class ReadmeHandler
       extends AbstractHandler {
    @Override
    public Object execute(ExecutionEvent event)
          throws ExecutionException {
       IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
       Shell shell = window.getShell();
-      TitleAreaDialog dialog = new TitleAreaDialog(shell);
-      dialog.setTitle("Important Notes");
+      String title = "Important Notes";
       String message =
             "Be sure the current selection is a file in Git repository!"
-                  + "\n Otherwise you might see the message 'Command is not enabled."
-                  + "\n\n You must also defined the Eclipse String Substitution variable 'git_exec.  The value should be"
-                  + " the absolute path to your Git executable.";
-      dialog.setMessage(message);
-      dialog.open();
+                  + "\n Otherwise you might see the message 'Command is not enabled'."
+                  + "\n\n You must also define the Eclipse String Substitution variable 'git_exec'. " 
+                  + "\n Do this in Preferences -> Run/Debug -> String Substitution." 
+                  + "\nThe value should be the absolute path to your Git executable.";
+      MessageDialog.open(MessageDialog.INFORMATION, shell, title, message, MessageDialog.NONE);
       return null;
    }
 }
