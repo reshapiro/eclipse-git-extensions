87272853616e16f6e33955f634f89afe840f35d3
R Shapiro <rshapiro@bbn.com>
2014-03-15 12:37:07 -0400

    Drop path parameter in checkout command, egit handles this now.

--- plugin/src/org/res/gitx/handlers/CheckoutCommand.java
+++ plugin/src/org/res/gitx/handlers/CheckoutCommand.java
@@ -1,12 +1,10 @@
 package org.res.gitx.handlers;
 
-import org.res.gitx.parameter.CheckBoxParameter;
 import org.res.gitx.parameter.MissingRequiredParameterException;
 import org.res.gitx.parameter.Parameter;
 import org.res.gitx.parameter.ParameterSet;
 import org.res.gitx.parameter.PromptCancelledException;
 import org.res.gitx.parameter.RefParameterWithDefault;
-import org.res.gitx.util.Resolver;
 
 /**
  * Git checkout, typically faster than EGit for large repositories
@@ -18,24 +16,16 @@ import org.res.gitx.util.Resolver;
 public class CheckoutCommand
       extends GitCommandHandler {
 
-   private static final Parameter PATH = new CheckBoxParameter("Selected path only", "selected path", false);
    private static final Parameter REF = new RefParameterWithDefault("HEAD", "Checkout");
    
-   private static final ParameterSet PARAMS = new ParameterSet("Checkout", PATH, REF);
+   private static final ParameterSet PARAMS = new ParameterSet("Checkout", REF);
 
    @Override
    void getArgs() 
          throws PromptCancelledException, MissingRequiredParameterException {
      promptForParameters(PARAMS);
      addArg("checkout");
-     
      addArg(PARAMS, REF);
-     
-     boolean usePath = PARAMS.getBooleanParameterValue(PATH);
-     String relativePath = usePath ? Resolver.resolveVariable("git_repo_relative_path") : null;
-     if (relativePath != null && !relativePath.isEmpty()) {
-        addArgs("--", relativePath);
-     }
    }
 
    @Override
