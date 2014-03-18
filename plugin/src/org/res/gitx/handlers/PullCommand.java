aadd556924ec6cde61ab420b05292214556c95fb
R Shapiro <rshapiro@bbn.com>
2014-03-18 14:39:19 -0400

    Remove -p optio from pull

--- plugin/src/org/res/gitx/handlers/PullCommand.java
+++ plugin/src/org/res/gitx/handlers/PullCommand.java
@@ -1,10 +1,5 @@
 package org.res.gitx.handlers;
 
-import org.res.gitx.parameter.CheckBoxParameter;
-import org.res.gitx.parameter.MissingRequiredParameterException;
-import org.res.gitx.parameter.Parameter;
-import org.res.gitx.parameter.ParameterSet;
-import org.res.gitx.parameter.PromptCancelledException;
 
 /**
  * Simple pull from upstream. This is here because it can sometimes be
@@ -17,18 +12,9 @@ import org.res.gitx.parameter.PromptCancelledException;
 public class PullCommand
       extends GitCommandHandler {
 
-   private static final Parameter PRUNE = new CheckBoxParameter("Pull", "Prune", true);
-
-   private static final ParameterSet PARAMS = new ParameterSet("Pull", PRUNE);
-
    @Override
-   void getArgs()
-         throws PromptCancelledException, MissingRequiredParameterException {
-      promptForParameters(PARAMS);
+   void getArgs() {
       append("pull");
-      if (PARAMS.getBooleanParameterValue(PRUNE)) {
-         append("-p");
-      }
    }
 
    @Override
