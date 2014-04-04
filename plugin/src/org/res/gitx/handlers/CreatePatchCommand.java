6dae446f7b641b3be356dd4085fac73b25ff9c4c
R Shapiro <rshapiro@bbn.com>
2014-04-04 09:57:34 -0400

    Patch cration logic requires -R option

--- plugin/src/org/res/gitx/handlers/CreatePatchCommand.java
+++ plugin/src/org/res/gitx/handlers/CreatePatchCommand.java
@@ -10,7 +10,12 @@ import org.res.gitx.parameter.RefParameter;
 import org.res.gitx.parameter.SaveFileParameter;
 
 /**
- * Execute the Git operation that will create a patch file
+ * Execute the Git operation that will create a patch file.
+ * You need to select another reference to patch against.
+ * 
+ * The assumption is that it will be run when the current branch
+ * is the one where you will ultimately apply the patch, and
+ * the selected reference is where you will be patching from.
  * 
  * @author reshapiro
  * 
@@ -19,7 +24,7 @@ public class CreatePatchCommand
       extends GitCommandHandler {
 
    private static final Parameter PATCH_FILE_PARAM = new SaveFileParameter("Save patch as", true);
-   private static final RefParameter REF = new RefParameter("target");
+   private static final RefParameter REF = new RefParameter("source");
    private static final ParameterSet PARAMETERS = new ParameterSet("Create patch file", PATCH_FILE_PARAM, REF);
 
    private String patchFilePath;
@@ -28,7 +33,7 @@ public class CreatePatchCommand
    void getArgs()
          throws PromptCancelledException, MissingRequiredParameterException {
       promptForParameters(PARAMETERS);
-      append("diff").append(PARAMETERS.getParameterValue(REF));
+      append("diff").append("-R").append(PARAMETERS.getParameterValue(REF));
       patchFilePath = PARAMETERS.getParameterValue(PATCH_FILE_PARAM);
    }
 
